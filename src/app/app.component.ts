b878485d33bfeb5b0f4293430ef7d768c88e08c2
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-05-12 01:35:42 +0200

    angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+    {value: 'chapati-3', viewValue: 'Chapati'},
+    {value: 'chicken-5', viewValue: 'Chicken'},
+  ];
+  food = 'chapati-3';
+  myvalue = 2;
+  isCheched = false;
+
+  onChange($event) {
+    console.log($event);
+  }
+
   title = 'angular-scratch by hb';
   courses = ['course1', 'course2', 'course3'];
   viewMode = 'map';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import {CoursesComponent} from './courses.component';
 import { CourseComponent } from './course/course.component';
-import {CoursesService} from './courses.service';
+import { CoursesService } from './courses.service';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +27,15 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [
     CoursesService
